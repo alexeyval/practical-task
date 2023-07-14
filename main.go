38cfc457f5e31eb6b391855eb388ac4e5d484936
Alexey Valitov <alexeyvalitov28@gmail.com>
2023-07-14 12:41:38 +0500

    03.6.01

--- main.go
+++ main.go
@@ -3,5 +3,35 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Тут будет выведен идентификатор")
+	checks := GenerateCheck()
+	for _, service := range checks {
+		if service.status == PassStatus {
+			fmt.Println(service.ServiceID)
+		}
+	}
+}
+
+type HealthCheck struct {
+	ServiceID int
+	status    string
+}
+
+const (
+	PassStatus = "pass"
+	FailStatus = "fail"
+)
+
+func GenerateCheck() (checks []HealthCheck) {
+	for i := 0; i < 5; i++ {
+		check := HealthCheck{
+			ServiceID: i,
+		}
+		check.status = FailStatus
+		if i%2 == 0 {
+			check.status = PassStatus
+		}
+		checks = append(checks, check)
+	}
+
+	return checks
 }
